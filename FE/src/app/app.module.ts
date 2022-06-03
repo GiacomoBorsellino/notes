ab1c1d163c8687b0fb197e5dec9dfc0070ce9d61
GiacomoBorsellino <giacomoborsellino@hotmail.it>
2022-06-03 12:28:50 +0200

    added material, need fix spacing books and fixresponsive

--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntrypointComponent } from './entrypoint/entrypoint.component';
 import { NotesComponent } from './notes/notes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop'
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NotesComponent } from './notes/notes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FE/src/app/notes/notes.component.ts
+++ FE/src/app/notes/notes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-notes',
@@ -8,6 +10,37 @@ import { Component, OnInit } from '@angular/core';
 export class NotesComponent implements OnInit {
 
   constructor() { }
+
+  timePeriods = [
+    'Bronze age',
+    'Iron age',
+    'Middle ages',
+    'Early modern period',
+    'Long nineteenth century',
+  ];
+
+  books = [
+    "../../assets/books/book1.png",
+    "../../assets/books/book2.png",
+    "../../assets/books/book3.png",
+    "../../assets/books/book4.png",
+    "../../assets/books/book5.png",
+    "../../assets/books/book6.png",
+    "../../assets/books/book7.png",
+    "../../assets/books/book8.png"
+  ];
+
+  clicking(name: string) {
+    console.log(name)
+  }
+
+  dropped(event: CdkDragDrop<string[]>) {
+    moveItemInArray(
+       this.books,
+       event.previousIndex,
+       event.currentIndex
+      );
+    }
 
   ngOnInit(): void {
   }
